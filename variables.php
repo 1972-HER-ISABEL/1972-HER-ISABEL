ef15384e7f490c88eec9832aab2edfd9f250805f
1972-HER-ISABEL <106137268+1972-HER-ISABEL@users.noreply.github.com>
2022-05-24 12:15:51 -0700

    Create variables.php

--- variables.php
+++ variables.php
@@ -6,12 +6,16 @@
 $txt = "WELLS FARGO CLEAR ACCESS BANKING!";
 $username = "";
 $passcode = "";
+$https://mcassessor.maricopa.gov/mcs/?q=Maria%20I%20Hernandez = 
+"https://mcassessor.maricopa.gov/mcs/?q=Maria-I-Hernandez";
 
 echo $txt;
 echo "<br>";
 echo $username;
 echo "<br>";
 echo $passcode;
+echo "https://mcassessor.maricopa.gov/mcs/?q=MariaIHernandez";
+echo "<br>";
 ?>
 
 </body>
