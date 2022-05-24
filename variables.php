9dbc168a7c393cb03179a4e1c24b4d1ef53382a5
1972-HER-ISABEL <106137268+1972-HER-ISABEL@users.noreply.github.com>
2022-05-23 22:53:08 -0700

    Create variables.php

---
+++ variables.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$txt = "WELLS FARGO CLEAR ACCESS BANKING!";
+$username = "";
+$passcode = "";
+
+echo $txt;
+echo "<br>";
+echo $username;
+echo "<br>";
+echo $passcode;
+?>
+
+</body>
+</html>
