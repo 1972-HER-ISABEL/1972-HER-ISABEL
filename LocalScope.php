49f94bb573f11afdd68bec240cdb3a6ca233c396
1972-HER-ISABEL <106137268+1972-HER-ISABEL@users.noreply.github.com>
2022-05-23 23:03:51 -0700

    Create LocalScope.php

---
+++ LocalScope.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+function myTest(WELLS FARGO CLEAR ACCESS BANKING) {
+  $username = ""; // local scope
+  echo "<p>Variable "" inside function is: $username</p>";
+} 
+myTest(WELLS FARGO REAL APP OR WEBSITE);
+
+// using username outside the function will generate an error
+echo "<p>Variable ""
