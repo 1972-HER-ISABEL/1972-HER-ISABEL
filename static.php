77fe5958c89d3c16538031e502de46ba67c5c444
1972-HER-ISABEL <106137268+1972-HER-ISABEL@users.noreply.github.com>
2022-05-23 23:11:27 -0700

    Create static.php

---
+++ static.php
@@ -0,0 +1,20 @@
+!DOCTYPE html>
+<html>
+<body>
+
+<?php
+function myTest(WELLS FARGO CLEAR ACCESS BANKING) {
+  static $username = 0;
+  echo $username;
+  $username++;
+}
+  
+
+myTest(WELLS FARGO CLEAR ACCESS BANKING);
+echo "<br>";
+myTest(Single-sign-in);
+echo "<br>";
+myTest(no-other-users);
+?> 
+
+</body>
