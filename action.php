8c3e6e2556de676c9f12e1ec1f1f75405a60d6a8
1972-HER-ISABEL <106137268+1972-HER-ISABEL@users.noreply.github.com>
2022-05-25 18:56:03 -0700

    Create action.php

---
+++ action.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>HTML Forms</h2>
+
+<form action="/action_page.php">
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" value="Maria Isabel"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" value="Hernandez"><br><br>
+  <label for="WELLS FARGO CLEAR ACCESS BANKING">WELLS FARGO CLEAR ACCESS BANKING:</label>
+  <text type="text" id="WELLS FARGO CLEAR ACCESS BANKING" name="WELLS FARGO CLEAR ACCESS BANKING"
+  value="Chapolin35"><br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+<p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
+
+</body>
+</html>
+
